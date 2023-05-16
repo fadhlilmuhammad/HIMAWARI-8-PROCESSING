e0c946efd2331b2d267ae5daabb0d439a43d31eb
fadhlilmuhammad <120076362+fadhlilmuhammad@users.noreply.github.com>
2023-05-16 17:08:38 +1000

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-proj="w40"
-uname="fm6730"
+proj="w40"                                                        #Change to your project
+uname="xx1234"                                                    #Change to your username
 
-foldername="H8_TUTORIAL"
+foldername="H8_TUTORIAL"                                          #Don't change
 
-mkdir /g/data/${proj}/${uname}/${foldername}
-cp -r /g/data/v46/fm6730/H8_TUTORIAL /g/data/${proj}/${uname}/
+mkdir /g/data/${proj}/${uname}/${foldername}                      #make folder
+cp -r /g/data/v46/fm6730/H8_TUTORIAL /g/data/${proj}/${uname}/    #copy folder
